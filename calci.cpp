5400aec221678222dddd5be5f05b8a2b3b70d54c
shivani chauhan <shivanichauhan0116@gmail.com>
2019-04-01 19:01:53 +0530

    main body

--- calci.cpp
+++ calci.cpp
@@ -12,4 +12,64 @@ public:
     int Multiplication(int x, int y);       
     int Division(int x, int y);
 };
+int main(){
+int func;
+int x, y;
 
+functions key; //Object
+key.Body(); //Object
+
+cout << "What function do you want to use? " << endl;
+cout << "1 - Addition " << endl;
+cout << "2 - Subtraction " << endl;
+cout << "3 - Multiplication " << endl;
+cout << "4 - Division " << endl;
+cout << "Input: " << endl;
+
+cin >> func;
+cout << endl;
+
+switch(func){
+
+    case 1: //Addition
+        cout << "**ADDITION**" << endl;
+        cout << "Please enter first number: " << endl;
+        cin >> x;
+        cout << "Please enter second number: " << endl;
+        cin >> y;
+        cout << x << " + " << y << " = "; 
+        cout << key.Addition(x, y);
+        break;          
+    case 2: //Subtraction
+        cout << "**SUBTRACTION**" << endl;  
+        cout << "Please enter first number: " << endl;
+        cin >> x;
+        cout << "Please enter second number: " << endl;
+        cin >> y;
+        cout << x << " - " << y << " = ";
+        cout << key.Subtraction(x, y);
+        break;              
+    case 3: //Multiplication
+        cout << "**MULTIPLICATION**" << endl;   
+        cout << "Please enter first number: " << endl;
+        cin >> x;
+        cout << "Please enter second number: " << endl;
+        cin >> y;
+        cout << x << " x " << y << " = ";
+        cout << key.Multiplication(x, y);
+        break;      
+    case 4: //Division
+        cout << "**DIVISION**" << endl; 
+        cout << "Please enter first number: " << endl;
+        cin >> x;
+        cout << "Please enter second number: " << endl;
+        cin >> y;
+        cout << x << " / " << y << " = ";
+        cout << key.Division(x, y);
+        break;
+    default:
+        cout << "Invalid Input...";
+        break;
+}
+
+}
