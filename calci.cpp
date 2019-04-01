9d8b897db4b825768c5ec438e607112d13e21bdb
shivani chauhan <shivanichauhan0116@gmail.com>
2019-04-01 18:59:12 +0530

    function declaration

---
+++ calci.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+#include <conio.h>
+using namespace std;
+
+class functions{
+public:
+    void Body(){
+        cout << " :: Welcome to Taylor's CALCULATOR! ::" << endl;   
+    }               
+    int Addition(int x, int y);
+    int Subtraction(int x, int y);
+    int Multiplication(int x, int y);       
+    int Division(int x, int y);
+};
+
